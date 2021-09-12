a1d4f3e51841cce6095377cfa7c8b21e68eacfd1
Sergio Virginio dos Santos <sergiologos6@gmail.com>
2021-09-11 23:58:48 -0300

    Update index.tsx
    Acessar dashboard

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
                     <hr />
                     <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
                     <Link className="btn btn-primary btn-lg" to="/dashboard">
-                        Acessar dashboar
+                        Acessar dashboard
                     </Link>
                 </div>
             </div>
